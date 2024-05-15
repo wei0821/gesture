72c1ba9c7462f0ce02873c83fcd915bef81e671f
wei0821 <harrylin149@gmail.com>
2024-05-15 16:16:47 +0800

    v3-PointerEvents

--- app/src/main/java/com/example/gesture/MainActivity.kt
+++ app/src/main/java/com/example/gesture/MainActivity.kt
@@ -3,12 +3,21 @@ package com.example.gesture
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.gesture.ui.theme.GestureTheme
 
@@ -22,7 +31,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    //Greeting("Android")
+                    PointerEvents()
                 }
             }
         }
@@ -30,17 +40,22 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    GestureTheme {
-        Greeting("Android")
+fun PointerEvents() {
+    var msg by remember { mutableStateOf("") }
+    Column {
+        Text(msg)
+        Box(
+            Modifier
+                .fillMaxSize()
+                .background(Color.Yellow)
+                .pointerInput(Unit) {
+                    awaitPointerEventScope {
+                        while (true) {
+                            val event = awaitPointerEvent()
+                            msg = "${event.type}, ${event.changes.first().position}"
+                        }
+                    }
+                }
+        )
     }
 }
